a78376a3573ee57ad5bca52f2a0cefe2b48942e4
Ushakov Michael <um.nix.user@gmail.com>
2019-09-13 09:54:12 +0300

    assignments tests

--- Basics/Assignments/assignments.v
+++ Basics/Assignments/assignments.v
@@ -4,24 +4,35 @@ module assignments(
     input wire rst,
     output reg[7:0] data_nba, 
     output reg[7:0] data_ba);
+	 
+reg [7:0] nba_buffer;
+reg [7:0] ba_buffer;
      
 always @(posedge clk)
 begin
     if(~rst)
+    begin
+        nba_buffer <= 0;
         data_nba <= 0;
+    end
     else
     begin
-        data_nba <= data_nba + 1;
+        nba_buffer <= nba_buffer + 1;
+        data_nba <= nba_buffer;
     end
 end
 
 always @(posedge clk)
 begin
     if(~rst)
+    begin
         data_ba = 0;
+        ba_buffer = 0;
+    end
     else
     begin
-        data_ba = data_ba + 1;
+        ba_buffer = ba_buffer + 1;
+        data_ba = ba_buffer;
     end
 end
      
