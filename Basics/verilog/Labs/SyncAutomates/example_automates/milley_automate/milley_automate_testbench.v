cdcc8874440081038c9c603e32141d1321901813
Ushakov Michael <um.nix.user@gmail.com>
2019-12-17 13:35:25 +0300

    basic test, is not fully testing digital automate

--- Basics/verilog/Labs/SyncAutomates/example_automates/milley_automate/milley_automate_testbench.v
+++ Basics/verilog/Labs/SyncAutomates/example_automates/milley_automate/milley_automate_testbench.v
@@ -25,6 +25,48 @@ wire [1:0] b;
 reg clk;
 reg reset;
 
-milley_automate uut(.clk(clk), .reset(reset), .)
+reg [31:0] counter;
+
+milley_automate uut(.clk(clk), .reset(reset), .a(a), .b(b));
+
+initial
+begin
+    clk <= 0;
+    reset <= 1;
+    counter <= 0;
+    # 400
+    reset <= 0;
+end
+
+always
+begin
+    #50 clk <= ~clk;
+    #100 counter <= counter + 1;
+    if (counter == 5)
+    begin
+        a <= 2'b11;  // A3, c -> C3, b -> b4
+    end
+    if (counter == 7)
+    begin
+        a <= 2'b01;  // A1, c->C4
+    end
+    // B state should not changed until a1
+    if (counter == 8)
+    begin
+        a <= 2'b00;
+    end
+    if (counter == 9)
+    begin
+        a <= 2'b01;
+    end
+    if (counter == 11)
+    begin
+        a <= 2'b11;
+    end
+    if (counter == 12)
+    begin
+        a <= 2'b11;
+    end
+end
 
 endmodule
